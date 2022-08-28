bc33e28ca36eeb91ff52be29f0761e81608c9e97
Arthur Noronha <arthurnoronha92@gmail.com>
2022-08-28 00:11:24 -0300

    RocketCoffee
    Desafio da rocketSeat, rocketCoffee concluído com sucesso.
    Mais incrementos com JavaScript e responsividade para 425px.

---
+++ RocketCoffee/index.js
@@ -0,0 +1,12 @@
+const openMenu = document.getElementById('menu-mobile')
+const buttonMenu = document.getElementById('button-menu')
+const ImgOpen = document.getElementById('img-open')
+const ImgClose = document.getElementById('img-close')
+
+buttonMenu.addEventListener('click', () =>{
+    openMenu.classList.toggle('disabled')
+    ImgOpen.classList.toggle('disabled')
+    ImgClose.classList.toggle('disabled')
+    ImgClose.
+    console.log('ola');
+})
